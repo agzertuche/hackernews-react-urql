3190f0ca55c09c028209118ccc645b59db7d49ed
Arturo De la Garza <agzertuche@gmail.com>
2019-10-23 18:11:42 -0500

    urql graphql completed

---
+++ src/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import { cacheExchange } from '@urql/exchange-graphcache';
+import { BrowserRouter } from 'react-router-dom';
+import { getToken } from './token';
+import { FEED_QUERY } from './components/LinkList';
+import {
+  Provider,
+  Client,
+  dedupExchange,
+  fetchExchange,
+  subscriptionExchange,
+} from 'urql';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+
+const cache = cacheExchange({
+  updates: {
+    Mutation: {
+      post: ({ post }, _args, cache) => {
+        const variables = { first: 10, skip: 0, orderBy: 'createdAt_DESC' };
+        cache.updateQuery({ query: FEED_QUERY, variables }, data => {
+          if (data !== null) {
+            data.feed.links.unshift(post);
+            data.feed.count++;
+            return data;
+          } else {
+            return null;
+          }
+        });
+      },
+    },
+    Subscription: {
+      newLink: ({ newLink }, _args, cache) => {
+        const variables = { first: 10, skip: 0, orderBy: 'createdAt_DESC' };
+        cache.updateQuery({ query: FEED_QUERY, variables }, data => {
+          if (data !== null) {
+            data.feed.links.unshift(newLink);
+            data.feed.count++;
+            return data;
+          } else {
+            return null;
+          }
+        });
+      },
+    },
+  },
+});
+
+const subscriptionClient = new SubscriptionClient('ws://localhost:4000', {
+  reconnect: true,
+  connectionParams: {
+    authToken: getToken(),
+  },
+});
+
+const client = new Client({
+  url: 'http://localhost:4000',
+  fetchOptions: () => {
+    const token = getToken();
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : '' },
+    };
+  },
+  exchanges: [
+    dedupExchange,
+    cache,
+    fetchExchange,
+    subscriptionExchange({
+      forwardSubscription: operation => subscriptionClient.request(operation),
+    }),
+  ],
+});
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider value={client}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
